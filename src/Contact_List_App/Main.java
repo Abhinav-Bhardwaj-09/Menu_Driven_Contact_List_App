9f84adcd6beaa55fa62a32d252b6361a7c176062
ABhinavRajput-theCreator <abhinav.bhardwaj_cs18@gla.ac.in>
2020-02-24 23:03:55 +0530

    Initial
    Signed-off-by: ABhinavRajput-theCreator <abhinav.bhardwaj_cs18@gla.ac.in>

--- src/Contact_List_App/Main.java
+++ src/Contact_List_App/Main.java
@@ -22,7 +22,7 @@ public class Main {
         while (fl) {
             int p =0;
             String e = "";
-            System.out.println("Welcome to DBC's Contact List App  ");
+            System.out.println("Welcome to Abhinav's Contact List App  ");
             System.out.println("Press 1 to add a new contact.");
             System.out.println("Press 2 to view all contacts.");
             System.out.println("Press 3 to search for a contact");
@@ -112,7 +112,7 @@ public class Main {
                 int item = obj.nextInt();
                 objcontacts.deleteElement(item);
             } else if (n == 5) {
-                System.out.println("Thanks for using DBC's Contact List App ");
+                System.out.println("Thanks for using Abhinav's Contact List App ");
                 fl = false;
             }
         }
